0930b64e31658d95d4c5984fcde2ad95e7826f86
circlle <2900797106@qq.com>
2021-03-10 18:57:30 +0800

    fix(server/nestjs): add data interceptor and httpError filter
    make data format more unified

---
+++ server/nestjs/src/core/http-exception.filter.ts
@@ -0,0 +1,44 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+export type ErrorResponse = {
+  statusCode: number;
+  data: { error: string };
+  message: string;
+  code: 1;
+  url: string;
+};
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost): any {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message = exception.message
+      ? exception.message
+      : `${status >= 500 ? 'Service Error' : 'Client Error'}`;
+
+    // TODO add logger
+    const errorResponse: ErrorResponse = {
+      statusCode: status,
+      data: { error: message },
+      message: 'failed',
+      code: 1,
+      url: request.originalUrl,
+    };
+    response.status(status);
+    response.header('Content-Type', 'application/json; charset=utf-8');
+    response.send(errorResponse);
+  }
+}
