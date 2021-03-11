807f98b7be288d30310c27f2d7059340274139b5
circlle <2900797106@qq.com>
2021-03-12 00:31:53 +0800

    feat(client/react-material): deal top level routes

--- client/react-material/src/App.tsx
+++ client/react-material/src/App.tsx
@@ -1,65 +1,27 @@
-import React, { useState } from "react";
-import {
-  AppBar,
-  CssBaseline,
-  makeStyles,
-  Paper,
-  Tab,
-  Tabs,
-} from "@material-ui/core";
-import {
-  StarOutlineOutlined,
-  CategoryOutlined,
-  ShoppingCartOutlined,
-  PersonOutlined,
-} from "@material-ui/icons";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BottomBar from "./components/shared/BottomBar";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    top: "auto",
-    bottom: 0,
-  },
-  root: {
-    flexGrow: 1,
-    maxWidth: 500,
-  },
-}));
 
 function App() {
-  const classes = useStyles();
-
   return (
     <Router>
-      <CssBaseline />
-      <Paper></Paper>
+      <Switch>
+        <Route path={'/category'}>
+          Category
+        </Route>
+        <Route path={"/shopping"}>
+          Shopping Cart
+        </Route>
+        <Route path={'/my'}>
+          cart
+        </Route>
+        <Route path={'/'}>
+          I am home
+        </Route>
+      </Switch>
       <BottomBar />
     </Router>
-  );
-}
-
-function BottomBar() {
-  const classes = useStyles();
-  const [indicator, setIndicator] = useState(0);
-
-  return (
-    <AppBar position={"fixed"} color={"primary"} className={classes.appBar}>
-      <Paper square className={classes.root}>
-        <Tabs
-          value={indicator}
-          onChange={(_, newValue) => setIndicator(newValue)}
-          variant={"fullWidth"}
-          indicatorColor={"secondary"}
-          textColor={"secondary"}
-        >
-          <Tab icon={<StarOutlineOutlined />} label={"首页"} />
-          <Tab icon={<CategoryOutlined />} label={"分类"} />
-          <Tab icon={<ShoppingCartOutlined />} label={"购物车"} />
-          <Tab icon={<PersonOutlined />} label={"我的"} />
-        </Tabs>
-      </Paper>
-    </AppBar>
   );
 }
 
